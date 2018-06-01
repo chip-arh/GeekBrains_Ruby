af518ae418d898efa72cc68137b7921e60c2debb
Chip <chip-arh@rambler.ru>
2018-06-02 01:58:35 +0300

    Task 1,2,3

---
+++ task1.rb
@@ -0,0 +1,55 @@
+# Task 1
+
+def getSymbol(c)    # функция возвращает стандартизованный символ или ошибку 
+    cod = c.upcase.ord;
+    if ((cod>=65)&(cod<=91))|((cod>=48)&(cod<=57))
+        c.upcase;
+    else
+        'Error';
+    end
+end
+
+str = ARGV[0]; # входной параметр
+if ((str.length >=1) & (str.length <=10000)) # проверка на соответствие
+    str = str.strip;                         # убираем пробелы с краев    
+    start_pos = 0;                           # начальная позиция для сравнения
+    end_pos = str.length()-1;                # конечная позиция для сравнения
+    #puts(str);
+    result = 'YES'                           # результат 
+    symb1='';                                # инициируем переменные
+    symb2='';                                #
+    while ((start_pos<end_pos)&(result=='YES')) #цикл пока позиции не сошлись и строка
+                                                #удовлетворяет условию
+        #puts('Start')
+        loop do                              # цикл пока не получим нормальный символ
+            #puts(start_pos)
+            symb1 = getSymbol(str[start_pos]);
+            if symb1 == 'Error'
+                start_pos=start_pos+1;
+            end
+        break if (symb1!='Error') | (start_pos>end_pos)
+        end
+        #puts('end')
+        loop do                             # цикл пока не получим нормальный символ
+            #puts(end_pos)
+            symb2 = getSymbol(str[end_pos]);
+            if symb2 == 'Error'
+                end_pos=end_pos-1;
+            end
+        break if (symb2!='Error') | (start_pos>end_pos)    
+        end
+        #puts("#{symb1} (#{start_pos}) - #{symb2} (#{end_pos})")
+        # проверяем символы
+        if ((symb1 != symb2) | (symb1 == 'Error'))
+            result = 'NO';
+        end
+        # сдвигаем позиции
+        start_pos = start_pos+1;
+        end_pos = end_pos-1;
+    end;
+    puts(result);
+else
+    puts('1 <= длина строки <= 10000');
+end;
+        
+
---
+++ task3.rb
@@ -0,0 +1,38 @@
+# Task 3
+
+def getSymbol(c)    # функция возвращает массив [глас, соглас, цифр] символ помещается в
+                    # соответсвующий элемент массива
+    cod = c.upcase.ord;
+    _num = '';
+    _glas = '';
+    _soglas = '';
+    if ((cod>=48)&(cod<=57)) # цифра
+        _num=c;
+    end
+    if ((cod>=65)&(cod<=91)) # символ
+        if (['A','E','I','O','U','Y'].index(c.upcase)!=nil) #гласный
+            _glas = c;
+        else 
+            _soglas = c;
+        end
+    end
+    return _glas, _soglas, _num 
+end
+
+str = ARGV[0]; # входной параметр
+if (str.length>=1)&(str.length<=10000)
+    # инициализация переменных
+    num='';
+    glas = '';
+    soglas = '';
+    # цикл посимвольно
+    str.each_char do |c|
+        r = getSymbol(c);       # получили массив
+        glas=glas+r[0];         # добавим в соответсвующие переменный
+        soglas = soglas+r[1];
+        num = num + r[2];
+    end
+    puts("#{glas} #{soglas} #{num}");
+else
+    puts("1 <= длина строки <= 10000 - не соблюдается")
+end
